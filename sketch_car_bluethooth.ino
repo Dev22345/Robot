889e3469ba06404591ab0febdf9d1e337f5e5177
Dev22345 <96528230+Dev22345@users.noreply.github.com>
2022-04-19 11:55:52 +0530

    Add files via upload

--- sketch_car_bluethooth.ino
+++ sketch_car_bluethooth.ino
@@ -1,11 +1,10 @@
 char t;
  
 void setup() {
-pinMode(13,OUTPUT);   //left motors forward
-pinMode(12,OUTPUT);   //left motors reverse
-pinMode(11,OUTPUT);   //right motors forward
-pinMode(10,OUTPUT);   //right motors reverse
-//pinMode(9,OUTPUT);   //Led
+pinMode(13,OUTPUT);   
+pinMode(12,OUTPUT);   
+pinMode(11,OUTPUT);   
+pinMode(10,OUTPUT);   
 Serial.begin(9600);
  
 }
@@ -16,32 +15,25 @@ if(Serial.available()){
   Serial.println(t);
 }
  
-if(t == 'F'){            //move forward(all motors rotate in forward direction)
+if(t == 'F'){            //move forward
   digitalWrite(13,HIGH);
   digitalWrite(11,HIGH);
 }
   
-else if(t == 'B'){      //move reverse (all motors rotate in reverse direction)
+else if(t == 'B'){      //move reverse
   digitalWrite(12,HIGH);
   digitalWrite(10,HIGH);
 }
  
-else if(t == 'L'){      //turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
+else if(t == 'L'){      //turn right
   digitalWrite(11,HIGH);
 }
  
-else if(t == 'R'){      //turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
+else if(t == 'R'){      //turn left
   digitalWrite(13,HIGH);
 }
-
-//else if(t == 'W'){    //turn led on or off)
-//  digitalWrite(9,HIGH);
-//}
-//else if(t == 'w'){
-//  digitalWrite(9,LOW);
-//}
  
-else if(t == 'S'){      //STOP (all motors stop)
+else if(t == 'S'){      
   digitalWrite(13,LOW);
   digitalWrite(12,LOW);
   digitalWrite(11,LOW);
